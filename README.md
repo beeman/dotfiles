e0e2bfe16bcc476aac7fc854143806a11264c17e
Bram Borggreve <borggreve@gmail.com>
2017-02-05 19:40:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# dotfiles
+
+> Bla bla my dotfiles bla bla
+
+
+
+
+## Installation
+
+
+curl https://cdn.rawgit.com/beeman/dotfiles/master/install.sh | sh
+
+## Credits
+
+- https://github.com/mhartington/dotfiles
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+brew="/usr/local/bin/brew"
+if [ -f "$brew" ]
+then
+  echo "Homebrew is installed, nothing to do here"
+else
+  echo "Homebrew is not installed, installing now"
+  echo "This may take a while"
+  echo "Homebrew requires osx command lines tools, please download xcode first"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+fi
+
+packages=(
+"git"
+"node"
+)
+
+for i in "${packages[@]}"
+do
+  brew install $i
+  echo "---------------------------------------------------------"
+done
+
+echo "installing RCM, for dotfiles management"
+brew tap thoughtbot/formulae
+brew install rcm
+echo "---------------------------------------------------------"
+
+localGit="/usr/local/bin/git"
+if [ -f "$localGit" ]
+then
+  echo "git is all good"
+else
+  echo "git is not installed"
+fi
+
+# Okay so everything should be good
+# Fingers cross at least
+# Now lets clone my dotfiles repo into .dotfiles/
+echo "---------------------------------------------------------"
+
+echo "Cloning beeman's dotfiles into .dotfiles"
+git clone https://github.com/beeman/dotfiles.git ~/.dotfiles
+
+cd .dotfiles
+git submodule update --init --recursive
+
+cd $HOME
+echo "running RCM's rcup command"
+echo "This is symlink the rc files in .dofiles"
+echo "with the rc files in $HOME"
+echo "---------------------------------------------------------"
+
+rcup
+
+echo "---------------------------------------------------------"
+
+echo "Changing to zsh"
+chsh -s $(which zsh)
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "You'll need to log out for this to take effect"
+echo "---------------------------------------------------------"
+
+exit 0
