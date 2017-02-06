d6c8f518a7fa2b33cedb8ab833f6772d0adeb444
Bram Borggreve <borggreve@gmail.com>
2017-02-05 22:00:07 -0500

    Optimize osx apps script

--- osx/apps.sh
+++ osx/apps.sh
@@ -25,24 +25,7 @@ apps=(
 # Specify the location of the apps
 appdir="/Applications"
 
-# Check for Homebrew
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
 main() {
-
-  # Ensure homebrew is installed
-  homebrew
-
-  # Install homebrew-cask
-  echo "installing cask..."
-  brew tap caskroom/cask
-  brew install brew-cask
-
-  # Tap alternative versions
-  brew tap caskroom/versions
 
   # install apps
   echo "installing apps..."
@@ -51,13 +34,6 @@ main() {
   # link with alfred
   alfred
   cleanup
-}
-
-homebrew() {
-  if test ! $(which brew); then
-    echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  fi
 }
 
 alfred() {
